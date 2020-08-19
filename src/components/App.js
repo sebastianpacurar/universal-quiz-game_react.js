709081a541c124041b92967eef97b199f7c45960
me <pacurar.sebastian90@gmail.com>
2020-08-20 01:22:29 +0300

    implement Question Section and major improvements:   - added 2 more states, related to question numbers and user answers   - implemented loading spinner when waiting for fetch results   - improved button which handles the gameSelect options   - implemented Question Section:     - answers and questions are displayed in order:       - when you hit next question, the next question will be rendered     - answers highlighting:       - correct answers will be highlighted in green       - wrong answers will be highlighted in red and the right answer will be highlighted in green to show the correct answer to the user     - improved grid system for questions   - improved styling for all the project   - fixed issue with sass command in package.json   - renamed ./src/utils.js file to selectOptions.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,82 @@
 import React, {useState} from 'react';
 
 // utils related
-import {CHALLENGE_CATEGORY, TYPE, DIFFICULTY} from '../utils/utils';
+import {CHALLENGE_CATEGORY, TYPE, DIFFICULTY} from '../utils/selectOptions';
 
+// fetch method
 import {fetchQuestions} from '../API';
 
-function App() {
+const App = () => {
 
-    const [gameStatus, setGameStatus] = useState({
+    const [gameSelect, setGameSelect] = useState({
         category: "",
         difficulty: "",
         type: "",
-        questionsAmount: 10,
+        questionsAmount: "",
     });
     const [loading, setLoading] = useState(false);
     const [questions, setQuestions] = useState([]);
+    const [questionNo, setQuestionNo] = useState(0);
     const [gameStarted, setGameStarted] = useState(false);
+    const [userAnswers, setUserAnswers] = useState(0);
 
 
     const handleStatus = (e) => {
         const key = e.target.id.split('-')[0];
         const value = e.target.value;
 
-        setGameStatus({
-            ...gameStatus, [key]: value
+        setGameSelect({
+            ...gameSelect, [key]: value
         })
     }
 
-
     const startGame = async () => {
+        setGameStarted(true);
         setLoading(true);
         const questions = await (fetchQuestions(
-            gameStatus.questionsAmount,
-            gameStatus.category,
-            gameStatus.difficulty,
-            gameStatus.type
+            gameSelect.questionsAmount,
+            gameSelect.category,
+            gameSelect.difficulty,
+            gameSelect.type
         ));
 
         setQuestions(questions);
-
         setLoading(false);
-        setGameStarted(true);
+    }
+
+    // TODO: need to fix this
+    const nextQuestion = () => {
+        setQuestionNo(questionNo + 1);
+
+        if (questionNo === userAnswers) {
+            setGameStarted(false);
+        }
+    }
+
+    /*
+        I used event bubbling here to be able to mark the right answer green and the user selected answer
+          red. In case the user selects the right answer, then only the right answer will be marked
+    */
+    const checkAnswer = (e) => {
+        const value = e.target.textContent
+        const answers = e.target.parentNode.children;
+
+        e.target.style.border = '1px solid #8a2be2';
+
+        if (value === questions[questionNo].correct_answer) {
+            e.target.style.backgroundColor = 'green';
+            e.target.style.color = 'white';
+        } else {
+            e.target.style.backgroundColor = 'red';
+            e.target.style.color = 'white';
+
+            for (let event of answers) {
+                if (event.textContent === questions[questionNo].correct_answer) {
+                    event.style.backgroundColor = 'green';
+                    event.style.color = 'white';
+                }
+            }
+        }
     }
 
     return (
@@ -48,42 +84,51 @@ function App() {
 
             <h1>Universal Quiz Game</h1>
 
+            {/* if loading state is true display loading spinner*/}
+            {loading && (
+                <article>
+                    <section className='spinner'>
+                        <span className='spinner-inner-1'/>
+                        <span className='spinner-inner-2'/>
+                        <span className='spinner-inner-3'/>
+                    </section>
+                </article>
+            )}
 
-            {!gameStarted && (
+            {/* if gameStarted state is false or all questions have been answered show game status*/}
+            {!gameStarted || userAnswers.length === parseInt(gameSelect.questionsAmount) ? (
 
-                <section className='game-status'>
+                <section id='game-select'>
 
-                    <select id='amount-select' onChange={handleStatus}>
-                        {Array.from(Array(50), (x, index) => index + 1).map((number) => (
-                                <option
-                                    label={number}
-                                    value={number}
-                                    key={`number - ${number}`}
-                                />
-                            )
-                        )}
+                    <select id='questionsAmount-select' onChange={handleStatus}>
+                        <option disabled selected>Choose no of questions</option>
+                        {Array.from(Array(46), (x, index) => index + 5).map((number) => (
+                            <option
+                                label={number}
+                                value={number}
+                                key={`number - ${number}`}
+                            />
+                        ))}
                     </select>
 
                     <select id='category-select' onChange={handleStatus}>
                         {CHALLENGE_CATEGORY.map((category, index) => (
-                                <option
-                                    label={category.label}
-                                    value={category.value}
-                                    key={`${category.label}-${index}`}
-                                />
-                            )
-                        )}
+                            <option
+                                label={category.label}
+                                value={category.value}
+                                key={`${category.label}-${index}`}
+                            />
+                        ))}
                     </select>
 
                     <select id='difficulty-select' onChange={handleStatus}>
                         {DIFFICULTY.map((difficulty, index) => (
-                                <option
-                                    label={difficulty.label}
-                                    value={difficulty.value}
-                                    key={`${difficulty.label}-${index}`}
-                                />
-                            )
-                        )}
+                            <option
+                                label={difficulty.label}
+                                value={difficulty.value}
+                                key={`${difficulty.label}-${index}`}
+                            />
+                        ))}
                     </select>
 
                     <select id='type-select' onChange={handleStatus}>
@@ -97,15 +142,70 @@ function App() {
                         )}
                     </select>
 
-                    <button id='start-btn' onClick={startGame}>Start Game</button>
+                    <button
+                        className='btn'
+                        onClick={startGame}
+                        disabled={!Boolean(gameSelect.questionsAmount)}
+                    >
+                        Start Game
+                    </button>
 
                 </section>
 
+                /* if gameStarted state is true and or all questions have not been answered, then if loading is false
+                     display quiz section
+                 */
+            ) : !loading ? (
 
-            )}
+                <QuestionSection
+                    questions={questions}
+                    questionNo={questionNo}
+                    nextQuestion={nextQuestion}
+                    checkAnswer={checkAnswer}
+                />
+            ) : null
+            }
 
         </React.Fragment>
     );
 }
 
+const QuestionSection = props => {
+
+    const {questions, questionNo, nextQuestion, checkAnswer} = props;
+    const currentQuestion = questions[questionNo];
+    const possibleAnswers = [...currentQuestion.incorrect_answers, currentQuestion.correct_answer]
+
+    return (
+        <article id='quiz-section'>
+
+            <p>Question {questionNo + 1} out of {questions.length}</p>
+
+            {/* using dangerouslySetInnerHTML to be able to parse the symbols. it's like using innerHTML in vanilla JS
+                  NEVER USE THIS TYPE OF ATTRIBUTE IN REACT, because it is an opening to an XSS (cross-site scripting) attack,
+                  but to be able to have a workaround on SoloLearn, i chose this way.
+            */}
+            <section id='question' dangerouslySetInnerHTML={{__html: currentQuestion.question}}/>
+
+            <article id='answers-container'>
+                {possibleAnswers.map((answer, index) => (
+                    <section
+                        className='possible-answers'
+                        key={`${answer}-${index}`}
+                        onClick={checkAnswer}
+                        dangerouslySetInnerHTML={{__html: answer}}/>
+                ))}
+            </article>
+
+            <button
+                className='btn'
+                onClick={nextQuestion}
+                disabled={false}
+            >
+                {questionNo + 1 === questions.length ? 'Check Result' : 'Next Question'}
+            </button>
+
+        </article>
+    )
+}
 export default App;
