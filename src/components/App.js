e2a3a2307959a395fd81e8a11c4f99d43bcbb183
me <pacurar.sebastian90@gmail.com>
2020-08-19 17:09:06 +0300

    implement API call, add some style:   - implemented basic fetch call according to the provided game status   - reimplemented game status states to work as on object of states   - added 2 more states for the questions and gameStart   - added some style:     - for select tags     - for the start game button     - implemented spinner while waiting for fetch to return data

--- src/components/App.js
+++ src/components/App.js
@@ -3,42 +3,106 @@ import React, {useState} from 'react';
 // utils related
 import {CHALLENGE_CATEGORY, TYPE, DIFFICULTY} from '../utils/utils';
 
+import {fetchQuestions} from '../API';
 
 function App() {
 
-    const [category, setCategory] = useState(null);
-    const [difficulty, setDifficulty] = useState(null);
-    const [type, setType] = useState(null);
+    const [gameStatus, setGameStatus] = useState({
+        category: "",
+        difficulty: "",
+        type: "",
+        questionsAmount: 10,
+    });
     const [loading, setLoading] = useState(false);
+    const [questions, setQuestions] = useState([]);
+    const [gameStarted, setGameStarted] = useState(false);
 
-    const startGame = (e) => {
 
+    const handleStatus = (e) => {
+        const key = e.target.id.split('-')[0];
+        const value = e.target.value;
+
+        setGameStatus({
+            ...gameStatus, [key]: value
+        })
+    }
+
+
+    const startGame = async () => {
+        setLoading(true);
+        const questions = await (fetchQuestions(
+            gameStatus.questionsAmount,
+            gameStatus.category,
+            gameStatus.difficulty,
+            gameStatus.type
+        ));
+
+        setQuestions(questions);
+
+        setLoading(false);
+        setGameStarted(true);
     }
 
     return (
         <React.Fragment>
-            <select id='category-select'>
-                {CHALLENGE_CATEGORY.map((category, index) => (
-                        <option label={category.label} value={category.value} key={`${category}-${index}`}/>
-                    )
-                )}
-            </select>
 
-            <select id='type-select'>
-                {DIFFICULTY.map((difficulty, index) => (
-                        <option label={difficulty.label} value={difficulty.value} key={`${difficulty}-${index}`}/>
-                    )
-                )}
-            </select>
+            <h1>Universal Quiz Game</h1>
 
-            <select id='difficulty-select'>
-                {TYPE.map((type, index) => (
-                        <option label={type.label} value={type.value} key={`${type}-${index}`}/>
-                    )
-                )}
-            </select>
 
-            <button onClick={startGame}>Start Game</button>
+            {!gameStarted && (
+
+                <section className='game-status'>
+
+                    <select id='amount-select' onChange={handleStatus}>
+                        {Array.from(Array(50), (x, index) => index + 1).map((number) => (
+                                <option
+                                    label={number}
+                                    value={number}
+                                    key={`number - ${number}`}
+                                />
+                            )
+                        )}
+                    </select>
+
+                    <select id='category-select' onChange={handleStatus}>
+                        {CHALLENGE_CATEGORY.map((category, index) => (
+                                <option
+                                    label={category.label}
+                                    value={category.value}
+                                    key={`${category.label}-${index}`}
+                                />
+                            )
+                        )}
+                    </select>
+
+                    <select id='difficulty-select' onChange={handleStatus}>
+                        {DIFFICULTY.map((difficulty, index) => (
+                                <option
+                                    label={difficulty.label}
+                                    value={difficulty.value}
+                                    key={`${difficulty.label}-${index}`}
+                                />
+                            )
+                        )}
+                    </select>
+
+                    <select id='type-select' onChange={handleStatus}>
+                        {TYPE.map((type, index) => (
+                                <option
+                                    label={type.label}
+                                    value={type.value}
+                                    key={`${type.label}-${index}`}
+                                />
+                            )
+                        )}
+                    </select>
+
+                    <button id='start-btn' onClick={startGame}>Start Game</button>
+
+                </section>
+
+
+            )}
 
         </React.Fragment>
     );
