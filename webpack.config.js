401b996f6661e93183421200ca45db8285f0ffaf
Sebastian Pacurar <sebastianpacurar@users.noreply.github.com>
2020-08-24 17:03:54 +0300

    implement basic configuration of Webpack with Babel: (#11)
    implement basic configuration of Webpack with Babel:
      - executed react-scripts eject, to eject all the default configuration, deleted both npm package json files, and recreated them from scratch
      - implemented Babel with a .babelrc configuration file:
        - added the env and react presets
        - targets for env presets are the latest browsers
        - added transform-runtime babel plugin to handle es2017 async/await promises
      - implemented webpack with a webpack.config.js file:
        - rule to handle js/jsx with babel-loader
        - HtmlWebPackPlugin to be able to inject the content rendered by react inside the div tag with id="root"
        - enabled minimize for the html rule
        - handler for sass
        - configured webpack-dev-server to run on port 3000
        - webpack serves the output from the dist folder
      - udated README.md

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: {
+                            minimize: true
+                        }
+                    }
+                ]
+            },
+
+            {
+                test: /\.s[ca]ss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: path.resolve(__dirname, 'public/index.html'),
+            filename: 'index.html',
+            inject: 'body'
+        })
+    ],
+
+    devtool: 'source-map',
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        compress: true,
+        port: 3000
+    }
+};
